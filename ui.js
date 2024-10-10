541b309ab3e7c3838e0d24c3c52a95e767e426a4
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2024-10-10 20:38:53 +0100

    Added buttons to expand each pane
    Required changes to CSS and a new script file that's just for UI stuff.

---
+++ ui.js
@@ -0,0 +1,12 @@
+function focus(element) {
+	console.log(element)
+	var ui = document.getElementById('ui')
+
+	// The normal class means the UI is in 'three panes' mode and we can replace it with the element passed in.
+	if ( ui.classList.contains('normal') ) {
+		ui.className = element;		
+	} else {
+		// if UI doesn't have a 'normal' class then we're reverting to it.
+		ui.className = 'normal';		
+	}
+}
