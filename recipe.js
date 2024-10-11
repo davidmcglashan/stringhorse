3b0d411c4198a3e396e9db73a07112e21d804d5d
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2024-10-11 07:58:52 +0100

    Rudimentary error messages

--- recipe.js
+++ recipe.js
@@ -2,21 +2,30 @@
  * Executes the currently entered recipe.
  */
 function recipe() {
-	// Our working objects.
-	var source = document.getElementById('src').value.split('\n');
-	var result = []
+	// Our working object is a copy of the source text.
+	var result = document.getElementById('src').value.split('\n');
+	var recipe
 
 	// This is the main loop, then. Get the recipe text, split by newlines and parse each one ...
-	for ( let recipe of document.getElementById('rec').value.split('\n') ) {
-		if ( recipe.length > 0 ) {
-			source = window[recipe](source);
+	try {
+		for ( recipe of document.getElementById('rec').value.split('\n') ) {
+			if ( recipe.length > 0 ) {
+				result = window[recipe](result);
+			}
+		}
+	} catch( err ) {
+		result = []
+		if ( err instanceof TypeError ) {
+			result.push( recipe + ': unknown or badly configured command' )
+		} else {
+			result.push(err)
 		}
 	}
 
 	// Finished looping. Better print the results ...
 	var tout = document.getElementById('out')
 	tout.value = ''
-	for ( let line of source ) {
+	for ( let line of result ) {
 		tout.value = tout.value + line + '\n'
 	}
 }
@@ -29,5 +38,5 @@ var timerId = 0;
 
 recipetext.addEventListener("keyup", function(event) {
     clearTimeout(timerId);
-    timerId = setTimeout( recipe, 1000 );
+    timerId = setTimeout( recipe, 750 );
 });
